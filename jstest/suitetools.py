64feea20f1cbb3c7e6a2268cce1f5a150fcfb286
Alan Vitor <alan@corp.globo.com>
2011-06-26 00:06:46 -0300

    class SuiteRender e etc...

--- jstest/suitetools.py
+++ jstest/suitetools.py
@@ -1,6 +1,10 @@
 # coding: utf-8
 
 from django.conf import settings
+from django.utils import simplejson
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 
 class SuiteRender(object):
     
@@ -12,11 +16,71 @@ class SuiteRender(object):
         else:
             self.framework = 'qunit' # default framework
 
-    def get_apps_with_jstest(self):
-        pass
+    def _get_modules_with_jstest(self):
+        # exclude_apps = (
+        #     "django.contrib.auth",
+        #     'django.contrib.contenttypes',
+        #     'django.contrib.sessions',
+        #     'django.contrib.sites',
+        # )
+        # 
+        # for a in settings.INSTALLED_APPS:
+        #     if not a in exclude_apps:
+        #         apps.append(a)
+
+        apps, modules = [a for a in settings.INSTALLED_APPS], []
+        app = self.app
+        
+        if app != None:
+            if not app in apps:
+                return []
+            apps = [app]
+
+        for a in apps:
+            test_module_path = "%s.tests" % (a)
+            try:
+                module = getattr(__import__(test_module_path, locals(), globals(), ["js"], 3), "js")
+                modules.append(module)
+            except:
+                pass
+
+        return modules
+
+    def _get_jslibs(self, modules):
+        js = []
+        for m in modules:
+            json_file = "%s/jslibs.json" % (m.__path__[0])
+            try:
+                content = simplejson.loads(open(json_file, 'r').read())
+                for src in content[0]["libs"]:
+                    js.append(src)
+            except:
+                pass
+
+        return js
+
+    def _get_tests(self, modules):
+        tests = ""
+        for m in modules:
+            tests_file_path = "%s/%s/tests.js" % (m.__path__[0], self.framework)
+            try:
+                tests += open(tests_file_path, 'r').read()
+            except:
+                pass
+        
+        return tests
+
+    def to_browser(self, request):
+        modules = self._get_modules_with_jstest()
+        context = {
+            'app': self.app,
+            'jslibs': self._get_jslibs(modules),
+            'tests': self._get_tests(modules)
+        }
+        return render_to_response(
+            'jstest/%s/index.html' % self.framework, context,
+            context_instance = RequestContext(request) )
 
     def to_console(self):
         pass
-        
-    def to_browser(self):
-        pass
+
--- jstest/urls.py
+++ jstest/urls.py
@@ -1,7 +1,17 @@
-#-*- coding:utf-8 -*-
+# coding: utf-8
 
 from django.conf.urls.defaults import url, patterns
+import os
 
-urlpatterns = patterns('jstest.views',
-    url(r'^qunit/(?P<app>[\w_]+)?$', 'suite_render'),
+media_root = os.path.join(os.path.dirname(__file__), 'media')
+serve = 'django.views.static.serve'
+
+urlpatterns = patterns('',
+    # qunit
+    url(r'^qunit/(?P<app>[\w_]+)?$', 'jstest.views.suite_render', {'framework': 'qunit'}),
+    url(r'^qunit/qunit.js', serve, {'document_root': media_root, 'path': 'jstest/js/qunit/qunit.js'}, name='qunit_js'),
+    url(r'^qunit/qunit.css', serve, {'document_root': media_root, 'path': 'jstest/css/qunit/qunit.css'}, name='qunit_css'),
+    
+    # jasmine
+    url(r'^jasmine/(?P<app>[\w_]+)?$', 'jstest.views.suite_render', {'framework': 'jasmine'}),
 )
