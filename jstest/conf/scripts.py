bcbf6847a87afa766756b75adad34dbf4aab1954
Alan Vitor <alan@corp.globo.com>
2011-06-29 14:11:01 -0300

    correcoes gerais de path

--- jstest/conf/scripts.py
+++
@@ -1,5 +0,0 @@
-JS_LIB_PATHS = {
-    'jstest': [
-        'jstest/js/jstest.functions.js',
-    ],
-}
--- jstest/tests/js/qunit/tests.js
+++
@@ -1,3 +0,0 @@
-test("Test to add one more", function() {
-    equal(JsTest.add_one_more(1), 2);
-});
