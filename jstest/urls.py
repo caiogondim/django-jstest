365b1350c9c740f28cbfd32b9205eb7330e7e6c6
Alan Vitor <alan@corp.globo.com>
2011-06-29 14:08:44 -0300

    adicionando os arquivos do Jasmine

--- jstest/urls.py
+++ jstest/urls.py
@@ -3,15 +3,18 @@
 from django.conf.urls.defaults import url, patterns
 import os
 
-media_root = os.path.join(os.path.dirname(__file__), 'media')
+media_root = os.path.join(os.path.dirname(__file__), 'jstest_media')
 serve = 'django.views.static.serve'
 
 urlpatterns = patterns('',
     # qunit
     url(r'^qunit/(?P<app>[\w_]+)?$', 'jstest.views.suite_render', {'framework': 'qunit'}),
-    url(r'^qunit/qunit.js', serve, {'document_root': media_root, 'path': 'jstest/js/qunit/qunit.js'}, name='qunit_js'),
-    url(r'^qunit/qunit.css', serve, {'document_root': media_root, 'path': 'jstest/css/qunit/qunit.css'}, name='qunit_css'),
+    url(r'^qunit/qunit.js', serve, {'document_root': media_root, 'path': 'js/qunit/qunit.js'}, name='qunit_js'),
+    url(r'^qunit/qunit.css', serve, {'document_root': media_root, 'path': 'css/qunit/qunit.css'}, name='qunit_css'),
     
     # jasmine
     url(r'^jasmine/(?P<app>[\w_]+)?$', 'jstest.views.suite_render', {'framework': 'jasmine'}),
+    url(r'^jasmine/jasmine.js', serve, {'document_root': media_root, 'path': 'js/jasmine/jasmine.js'}, name='jasmine_js'),
+    url(r'^jasmine/jasmine-html.js', serve, {'document_root': media_root, 'path': 'js/jasmine/jasmine-html.js'}, name='jasmine_html_js'),
+    url(r'^jasmine/jasmine.css', serve, {'document_root': media_root, 'path': 'css/jasmine/jasmine.css'}, name='jasmine_css'),
 )
---
+++ myapp/tests/js/jasmine/tests.js
@@ -0,0 +1,7 @@
+describe("Example functions", function() {
+    
+  it("should add one more", function() {
+    expect( JsTest.add_one_more(1) ).toBe(2);
+  });
+  
+});
