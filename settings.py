2d299869313976a71b04666e18a61f4e6c7fb76b
Alan Vitor <alan@corp.globo.com>
2011-06-29 14:10:37 -0300

    servindo estaticos no projeto

--- settings.py
+++ settings.py
@@ -1,15 +1,19 @@
 # coding: utf-8
 
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASE_ENGINE = 'sqlite3'
 
-MEDIA_ROOT = ''
-MEDIA_URL = ''
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'mymedia')
+MEDIA_URL = '/mymedia/'
+ADMIN_MEDIA_PREFIX = '/myadminmedia/'
 
 ROOT_URLCONF = 'jstest_project.urls'
 
 INSTALLED_APPS = (
     'jstest',
+    'myapp',
 )
--- urls.py
+++ urls.py
@@ -1,6 +1,12 @@
 # coding: utf-8
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 urlpatterns = patterns('',
     (r'^', include('jstest.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^mymedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
