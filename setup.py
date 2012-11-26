beac4cf9d4b4b08add7a4daed5e574ec6cdd6668
Alan <alan@corp.globo.com>
2012-11-26 15:08:05 -0200

    mudando o nome para django-jstest no setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='Django JSTest',
+    name='django-jstest',
     description=u'Django app to run javascript unit tests',
     author='Entretenimento',
     author_email='entretenimento@corp.globo.com',
