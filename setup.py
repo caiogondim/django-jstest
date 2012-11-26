c5e7fc795de633dc3f3f23055e3576740d799f71
Alan <alan@corp.globo.com>
2012-11-26 15:04:26 -0200

    nova versão

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Entretenimento',
     author_email='entretenimento@corp.globo.com',
     url='http://ngit.globoi.com/django-jstest/django-jstest',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
 )
